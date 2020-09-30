24ca9ae5a947af6fe804ffee8202e428a81e535c
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-30 19:18:58 +0530

    [Surajmal Kumar] Master commit

---
+++ AddressBookMain.java
@@ -0,0 +1,11 @@
+//SURAJMAL KUMAR
+//Address Book Problem.
+
+//https://github.com/surajkumar7252/AddressBook.git
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+	
+}
